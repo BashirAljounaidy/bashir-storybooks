467ec8b303d1263967107b9c65b5a4329c16b4cb
BashirAljounaidy <bma.963@gmail.com>
2023-08-09 10:50:01 +0000

    last touch on the project

--- routes/index.js
+++ routes/index.js
@@ -1,32 +1,30 @@
-const express= require('express')
+const express = require('express')
 const router = express.Router()
-const {ensureAuth,ensureGuest} = require('../middleware/auth')
-const Story =require('../models/Story')
+const { ensureAuth, ensureGuest } = require('../middleware/auth')
 
-// @desc Login/Landing page
-// @route GET /
-router.get('/',ensureGuest,(req,res)=> {
-  res.render('login',{layout:'login'})
+const Story = require('../models/Story')
+
+// @desc    Login/Landing page
+// @route   GET /
+router.get('/', ensureGuest, (req, res) => {
+  res.render('login', {
+    layout: 'login',
+  })
 })
 
-// @desc Dashboard 
-// @route GET /dashboard
-
-router.get('/dashboard',ensureAuth,async (req,res)=> {
+// @desc    Dashboard
+// @route   GET /dashboard
+router.get('/dashboard', ensureAuth, async (req, res) => {
   try {
-    const stories = await Story.find({user:req.user.id}).lean()
-    res.render('dashboard', { 
+    const stories = await Story.find({ user: req.user.id }).lean()
+    res.render('dashboard', {
       name: req.user.displayName,
-      stories
-    }) 
+      stories,
+    })
+  } catch (err) {
+    console.error(err)
+    res.render('error/500')
   }
-  catch (err) 
-  {
-    console.log(err)
-    res.render('error/500') 
-  }
-
-
 })
 
-module.exports = router  
+module.exports = router
